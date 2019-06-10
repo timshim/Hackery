fa47fb250e1a21cecbd3453bc341bee8beb1f638
Tim Shim <timshim@gmail.com>
2019-06-10 13:53:21 +0800

    Initial Commit

---
+++ Hackery/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Hackery
+//
+//  Created by Tim Shim on 6/10/19.
+//  Copyright © 2019 Tim Shim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
