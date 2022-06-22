27957451d987b3ebad4d4312265fde52a3bfa97f
Tim Shim <timshim@gmail.com>
2022-06-22 17:41:40 +0800

    Cleaned up FeedView into components, added support for swipe gesture to go back from SafariView

--- Hackery/Feed/FeedView.swift
+++ Hackery/Feed/FeedView.swift
@@ -15,58 +15,92 @@ struct FeedView: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                Color("background")
-                    .edgesIgnoringSafeArea(.all)
+                BackgroundView()
                 if viewModel.isLoading {
-                    VStack {
-                        Spacer()
-                        ProgressView()
-                        Spacer()
-                    }
-                    .edgesIgnoringSafeArea(.all)
+                    LoaderView()
                 }
-                ScrollView {
-                    LazyVStack {
-                        ForEach(viewModel.stories, id: \.id) { story in
-                            if let storyUrl = story.url, let url = URL(string: storyUrl) {
-                                NavigationLink(destination: SafariView(url: url)) {
-                                    StoryView(story: story)
-                                        .environmentObject(viewModel)
-                                }
-                            }
-                        }
-                        .cornerRadius(10)
-                        .padding(EdgeInsets(top: 0, leading: 8, bottom: -5, trailing: 8))
-                    }
-                }
-                .padding(.top, 50)
-                .edgesIgnoringSafeArea(.top)
-                VStack {
-                    Color("background")
-                        .frame(maxWidth: .infinity, minHeight: 50, idealHeight: 50, maxHeight: 50, alignment: .top)
-                    Spacer()
-                }
-                .edgesIgnoringSafeArea(.all)
-                VStack {
-                    Spacer()
-                    Button(action: {
-                        viewModel.loadTopStories()
-                    }) {
-                        Image(systemName: "arrow.clockwise")
-                            .font(.system(size: 24, weight: .bold, design: .rounded))
-                            .foregroundColor(Color("titleColor"))
-                            .padding(.bottom, 6)
-                    }
-                    .background(Circle().foregroundColor(Color("cardBg")).frame(width: 60, height: 60))
-                    .padding()
-                    .shadow(color: Color("shadow"), radius: 20, x: 0, y: 20)
-                }
+                ContentView(viewModel: viewModel)
+                StatusBarView()
+                ButtonView(tapped: viewModel.loadTopStories)
             }
             .navigationBarHidden(true)
         }
         .onAppear {
             viewModel.loadTopStories()
         }
+    }
+}
+
+struct BackgroundView: View {
+    var body: some View {
+        Color("background")
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var viewModel: FeedViewModel
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(viewModel.stories, id: \.id) { story in
+                    if let storyUrl = story.url, let url = URL(string: storyUrl) {
+                        NavigationLink(destination: SafariView(url: url)) {
+                            StoryView(story: story)
+                                .environmentObject(viewModel)
+                        }
+                    }
+                }
+                .cornerRadius(10)
+                .padding(EdgeInsets(top: 0, leading: 8, bottom: -5, trailing: 8))
+            }
+        }
+        .padding(.top, 50)
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct StatusBarView: View {
+    var body: some View {
+        VStack {
+            Color("background")
+                .frame(maxWidth: .infinity, minHeight: 50, idealHeight: 50, maxHeight: 50, alignment: .top)
+            Spacer()
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ButtonView: View {
+    var tapped: (() -> Void)?
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Button(action: {
+                tapped?()
+            }) {
+                Image(systemName: "arrow.clockwise")
+                    .font(.system(size: 24, weight: .bold, design: .rounded))
+                    .foregroundColor(Color("titleColor"))
+                    .padding(.bottom, 6)
+            }
+            .background(Circle().foregroundColor(Color("cardBg")).frame(width: 60, height: 60))
+            .padding()
+            .shadow(color: Color("shadow"), radius: 20, x: 0, y: 20)
+        }
+    }
+}
+
+struct LoaderView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            ProgressView()
+            Spacer()
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
--- Hackery/Feed/SafariView.swift
+++ Hackery/Feed/SafariView.swift
@@ -54,6 +54,21 @@ struct SafariViewController: UIViewControllerRepresentable {
     }
 }
 
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+    
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        true
+    }
+}
+
 struct SafariView_Previews: PreviewProvider {
     static var previews: some View {
         SafariView(url: URL(string: "https://google.com")!)
